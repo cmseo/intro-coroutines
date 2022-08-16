fe0caafd13e6d7f0e37067316fbac0d7795305bb
changmu seo <changmu.seo@lge.com>
2022-08-17 03:08:36 +0900

    by Concurrency
    concurrency

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -14,5 +14,17 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+/*fun List<User>.aggregate(): List<User> =
+    this*/
+
+
+
+
+fun List<User>.aggregate(): List<User> = groupBy { it.login }
+    .map { (login, group) ->
+        User(
+            login = login,
+            contributions = group.sumOf { it.contributions }
+        )
+    }
+    .sortedByDescending { it.contributions }
